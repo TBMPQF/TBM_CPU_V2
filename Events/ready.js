823ba3913769e7c287bbd41efdf0498d2e0d6342
TBMPQF <benjaminsourd@hotmail.fr>
2023-03-17 18:19:06 +0100

    Rabbyt

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ const { ActivityType } = require("discord.js");
 
 module.exports = {
   name: "ready",
-  execute(bot) {
+  execute(bot, member) {
     console.log(
       "\x1b[33m" + `${bot.user.username} connecté !\n` + "\x1b[33m" + ``
     );
@@ -10,6 +10,14 @@ module.exports = {
     bot.channels.cache
       .get(`838440585341566996`)
       .send(`**Je viens tout juste de redémarrer.**`);
+
+    setInterval(() => {
+      bot.channels.cache
+        .get(`717144491525406791`)
+        .send(
+          `@everyone. Bonjour à tous, n'oubliez pas de prendre votre \`Daily\``
+        );
+    }, 14400000);
 
     bot.user.setPresence({
       activities: [{ name: bot.config.activity, type: ActivityType.Playing }],
