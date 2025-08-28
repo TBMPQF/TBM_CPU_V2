83d4c3b0419affec627a57ea1bba6f363d54df3f
Nicolas-mahe <nicolas.mahe33@gmail.com>
2025-08-28 23:52:44 +0200

    gestion des redémmarage intenpestif

--- entrypoint-docker.sh
+++ entrypoint-docker.sh
@@ -8,7 +8,24 @@ if [ -d "./logs/current_error.log" ]; then
   mv ./logs/current_error.log ./logs/error.log
 fi
 
+# Gestion du delai de démarrage en cas de multiple reboot
+if [ ! -f ./nombre_redemmarage.txt ]; then
+  touch ./nombre_redemmarage.txt
+  echo 0 >> ./nombre_redemmarage.txt
+else
+  nombre_demmarage=$(tail -n 1 ./nombre_redemmarage.txt)
+  echo $((nombre_demmarage + 1)) >> ./nombre_redemmarage.txt
+fi
+nombre_redemmarage=$(tail -n 1 ./nombre_redemmarage.txt)
+delai=$((nombre_redemmarage * 1))
+if [ $delai -gt 120 ]; then
+  delai='120'
+fi
+echo "Démarrage multiple ($nombre_redemmarage) en cours, attente de $delai seconde(s)..."
+sleep $delai
+
 # Creation du fichier de configuration
+echo "Création du fichier de configuration..."
 cat << EOF > ./config.json
 {
   "token": "$TOKEN",
@@ -24,4 +41,5 @@ cat << EOF > ./config.json
 EOF
 
 # Démarrage du code avec Gestion des logs dans les fichiers et les traces de l'image docker en même temps
+echo "Démarrage du bot..."
 node /data/index.js > >(awk '{ print strftime("%d/%m_%Hh%Mm%Ss"), $0; fflush(); }' | tee -a ./logs/current_output.log) 2> >(awk '{ print strftime("%d/%m_%Hh%Mm%Ss"), $0; fflush(); }' | tee -a ./logs/current_error.log >&2)
