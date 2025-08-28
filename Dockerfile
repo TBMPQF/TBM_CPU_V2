5a1ce1349a387a1e6d6992de85ee7d1cbaf124d1
Nicolas-mahe <nicolas.mahe33@gmail.com>
2025-08-28 17:49:32 +0200

    update image

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM node:22.6.0-alpine
 LABEL maintainer="TBMPQF"
 
-# Install dependencies
-RUN apk add --no-cache ffmpeg git py3-pip
-
-# Set workdir
+# Definition du repertoire de travail dans l'image docker
 WORKDIR /data
 
-# Copy local files (optionally, if you want to build from local context)
-COPY . /data
+# Copie des fichiers locaux dans l'image
+COPY . .
+
+# Installation des dependances
+RUN apk add --no-cache ffmpeg git py3-pip
 
 # Make config script executable
-RUN chmod +x /data/CreateConfig.sh || true
+RUN chmod +x ./entrypoint-docker.sh
 
 # Entrypoint (can be overridden by docker-compose)
-CMD sh -c "/data/CreateConfig.sh && node /data/index.js > >(awk '{ print strftime(\"%d/%m_%Hh%Mm%Ss\"), $0; fflush(); }' | tee -a /data/logs/current_output.log) 2> >(awk '{ print strftime(\"%d/%m_%Hh%Mm%Ss\"), $0; fflush(); }' | tee -a /data/logs/current_error.log >&2)"
+CMD sh -c "./entrypoint-docker.sh"
---
+++ entrypoint-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Gestion des fichiers de Logs
+if [ -d "./logs/current_output.log" ]; then
+  mv ./logs/current_output.log ./logs/output.log
+fi
+if [ -d "./logs/current_error.log" ]; then
+  mv ./logs/current_error.log ./logs/error.log
+fi
+
+# Creation du fichier de configuration
+cat << EOF > ./config.json
+{
+  "token": "$TOKEN",
+  "mongourl": "$BDD_URL",
+  "serveurMinecraftDOMAIN": "$MINECRAFT_SERVER_URL",
+  "twitch": {
+    "clientId": "$TWITCH_ID",
+    "clientSecret": "$TWITCH_SECRET"
+  },
+  "apex_api": "$APEX_API",
+  "genius_api": "$GENIUS_API"
+}
+EOF
+
+# Démarrage du code avec Gestion des logs dans les fichiers et les traces de l'image docker en même temps
+node /data/index.js > >(awk '{ print strftime(\"%d/%m_%Hh%Mm%Ss\"), $0; fflush(); }' | tee -a /data/logs/current_output.log) 2> >(awk '{ print strftime(\"%d/%m_%Hh%Mm%Ss\"), $0; fflush(); }' | tee -a /data/logs/current_error.log >&2)"
